1e90bb42ff94b2f7956d5f9e43368306781de95b
Desha <mostafamohamed92895@gmail.com>
2026-09-22 23:43:33 +0300

    edit template joblist to be modified with slug

--- job/views.py
+++ job/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Job
 
@@ -5,11 +6,15 @@ from .models import Job
 
 def job_list(request):
     job_list = Job.objects.all()
-    context ={'Jobs' : job_list}
+    paginator = Paginator(job_list,1)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context ={'Jobs' : page_obj}
     return render(request,'Job/job_list.html', context=context)
      
 
-def job_detail(request, id):
-    job_detail = Job.objects.get(id = id)
+def job_detail(request, slug):
+    job_detail = Job.objects.get(slug = slug)
     context ={'Job' : job_detail}
     return render(request, 'Job/job_detail.html',context=context)
